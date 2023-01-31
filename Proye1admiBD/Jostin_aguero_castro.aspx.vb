3a8e805ca9489b4b8c6982bae39c36b7bfa5b240
Jostin Aguero Castro <jalexisac@hotmail.com>
2023-01-31 15:04:53 -0600

    Agregar Jostin

---
+++ Proye1admiBD/Jostin_aguero_castro.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class Jostin_aguero_castro
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btn_cambiarColor_Click(sender As Object, e As EventArgs) Handles btn_cambiarColor.Click
+        lbl_Jostin.ForeColor = System.Drawing.Color.Aqua
+    End Sub
+End Class
